9ad0f3216f3e8a269e914129be006517c634dc35
Sangkyung <sangkyung.ha@navercorp.com>
2022-10-05 15:12:42 +0900

    fix api address

--- src/api/webApi.ts
+++ src/api/webApi.ts
@@ -5,7 +5,7 @@ import { BoardlifeBoardgame } from '../types/type';
 // axios.defaults.withCredentials = true;
 
 export const getBoardgamesApi = async (query: string): Promise<BoardlifeBoardgame[]> => {
-    const res = await axios.get(`/get_auto_search.php?word=${utfToEuckr(query)}`, {
+    const res = await axios.get(`https://boardlife.co.kr/get_auto_search.php?word=${utfToEuckr(query)}`, {
         withCredentials: true,
     });
     return res.data.slice(0, 15);
